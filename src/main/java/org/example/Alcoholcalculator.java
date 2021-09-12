45501d38badb3f48f7e60248ae135591148c6223
SmokeIce593 <37118809+SmokeIce593@users.noreply.github.com>
2021-09-12 18:37:35 -0400

    Fix

--- src/main/java/org/example/Alcoholcalculator.java
+++ src/main/java/org/example/Alcoholcalculator.java
@@ -13,16 +13,32 @@ public class Alcoholcalculator {
         double BAC = 0;
 
         System.out.print("Enter a 1 if you are male or a 2 if you are female: ");
+        while (!input.hasNextInt()) {
+            input.nextLine();
+        }
         int gender = input.nextInt();
 
         System.out.print("How many ounces of alcohol did you have? ");
+        while (!input.hasNextInt()) {
+            input.nextLine();
+        }
         int consumption = input.nextInt();
 
+
         System.out.print("What is your weight, in pounds? ");
+        while (!input.hasNextInt()) {
+            input.nextLine();
+        }
         int weight = input.nextInt();
 
+
         System.out.print("How many hours has it been since your last drink? ");
+        while (!input.hasNextInt()) {
+            input.nextLine();
+        }
         int time = input.nextInt();
+
+
 
         if (gender == 1){
             BAC = 0.73 * (consumption * 5.14)/(weight) -0.015 * time;
@@ -34,15 +50,15 @@ public class Alcoholcalculator {
             System.out.printf("Your BAC is %.6f", BAC);
 
             if (BAC >= 0.08) {
-                System.out.printf("It is not legal for you to drive");
+                System.out.print("It is not legal for you to drive");
             }
         }
 
         if (BAC >= 0.08){
-            System.out.printf("\nIt is not legal for you to drive");
+            System.out.print("\nIt is not legal for you to drive");
         }
         else {
-            System.out.printf("\n1It is legal for you to drive");
+            System.out.print("\n1It is legal for you to drive");
         }
 
     }
